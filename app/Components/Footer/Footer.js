cba1a773ed4d278994f02a4034bfc518e082439a
Jason Sinn <jrs341@me.com>
2017-02-03 16:44:08 -0600

    added a footer with social buttons and admin button

--- app/Components/Footer/Footer.js
+++ app/Components/Footer/Footer.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router'
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton'
-// var helpers = require("../utils/helpers");
+import FontAwesome from 'react-fontawesome'
 
+const twitter = <FontAwesome name='twitter' size='2x'> </FontAwesome>;
+const google = <FontAwesome name='google' size='2x'> </FontAwesome>;	      	 
+const facebook = <FontAwesome name='facebook' size='2x'> </FontAwesome>;
+		        
 export default class Footer extends React.Component {
-	handleClick() {
-		alert("clicked Admin");
-		// need to goto Admin Login page here
-	}
-
+	
 	render() {
 		return (
-			<div> Soon to be footer 
-				<Link to={'admin'}>
-                <RaisedButton
-                  label="Go to Admin"
-                  primary={true}
-                />
-              </Link>
-			</div>
+			<Paper zDepth={1}>
+				<BottomNavigation>
+					<BottomNavigationItem
+						icon={twitter}
+					/>
+					<BottomNavigationItem
+						icon={google}
+					/>
+					<BottomNavigationItem
+						icon = {facebook} 
+					/>
+				</BottomNavigation>
+				
+					<Link to={'admin'}>
+		                <RaisedButton
+		                  label="Go to Admin"
+		                  primary={true}
+		                />
+	              	</Link>
+              	
+			</Paper>
 		);
 	}
 }
-

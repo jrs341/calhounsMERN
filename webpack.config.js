e13130703a76ebf981bf6ff0027b066f3317b7a6
Jason Sinn <jrs341@me.com>
2017-03-16 06:42:40 -0500

    updating config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var path = require("path");
+// var path = require("path");
 
 module.exports = {
   entry: "./app/app.js",
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/public/"
-    filename: "bundle.js"
+    // path: path.resolve(__dirname, "build"),
+    // publicPath: "/public/",
+    filename: "public/bundle.js"
   },
   module: {
     loaders: [
@@ -19,13 +19,13 @@ module.exports = {
         }
       }
     ]
-  },
+  }
   // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
   // Without this the console says all errors are coming from just coming from bundle.js
-  devtool: "eval-source-map",
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    hot: true
-  }
+  // devtool: "eval-source-map",
+  // devServer: {
+  //   historyApiFallback: true,
+  //   contentBase: './',
+  //   hot: true
+  // }
 };
